9c8d125f751c646e9c7f82586fa28924e9329ee0
Malcolm Peter <kraftycoder@gmail.com>
2018-08-14 15:12:05 +0300

    add the account function

--- get_contributors.py
+++ get_contributors.py
@@ -1,1 +1,16 @@
-# contributors.py
+import getpass
+from github import Github
+
+def get_account():
+    username = input("GitHub Username: ")
+    password = getpass.getpass('GitHub Password: ')
+
+    user = Github("".format(username), "".format(password))
+
+    for repo in user.get_user().get_repos():
+        print(repo.name)
+        repo.edit(has_wiki=False)
+
+
+get_account()
+
