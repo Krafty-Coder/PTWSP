51ffc487a9a3447a898ad9df5ef429daad9d1b98
Malcolm Peter <kraftycoder@gmail.com>
2018-08-14 23:44:36 +0300

    remove login from the command line to a page in html

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 import getpass
 from github import Github
-from flask import (Flask, logging, render_template,)
+from flask import (Flask, render_template, request, url_for, redirect)
 
 app = Flask(__name__)
 
 
 @app.route('/issues', methods=['GET'])
-def get_credentials():
-    username = input("GitHub Username: ")
-    password = getpass.getpass('GitHub Password: ')
-
+def get_issues():
     repos = []
     issues = []
-    user = Github(username, password)
+    # import pdb; pdb.set_trace()
+    authenticate_user()
+    user = authenticate_user.user
     repo = user.get_repo('python/python-docs-fr')
     for proj_issue in list(repo.get_issues()):
-        # import pdb; pdb.set_trace()
         issues.append(proj_issue.title)
 
     for repo in user.get_user().get_repos():
@@ -25,9 +23,22 @@ def get_credentials():
     return render_template('issues.html', issues=issues)
 
 
-# get_credentials()
+@app.route('/', methods=['POST', 'GET'])
+def authenticate_user():
+    # GEt form values
+    # import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        authenticate_user.user = Github(username, password)
+        print(request.form['username'])
+
+        return redirect(url_for('get_issues'))
+    else:
+        error = "Username not found"
+        return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
-    app.secret_key = 'secret_key_219641456885_krafty'
+    app.secret_key = '@sec#et_key_@#%2196414$6@8$_kr@fty#'
     app.run(debug=True)
